38707baa7bb04871d0bb2c8f8b49d603338025d3
Aleksa013 <velma86@bk.ru>
2024-10-02 13:07:39 +0300

    feat: fs/create is done

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import { stat } from "node:fs";
+import fs from "fs";
+
 const create = async () => {
-    // Write your code here 
+  stat("./src/fs/files/fresh.txt", (err, stats) => {
+    if (stats) {
+      throw new Error("FS operation failed");
+    }
+    if (err) {
+      fs.writeFile(
+        "./src/fs/files/fresh.txt",
+        "I am fresh and young",
+        (err) => {
+          if (err) throw err;
+        }
+      );
+    }
+  });
 };
 
 await create();
