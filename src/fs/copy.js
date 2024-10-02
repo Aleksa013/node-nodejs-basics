e14d86190c14b46f41106adc35d4e56f0283be71
Aleksa013 <velma86@bk.ru>
2024-10-02 13:49:01 +0300

    feat: fs/copy is done

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,22 @@
+import fs from "fs";
+import { stat } from "node:fs";
+
 const copy = async () => {
-    // Write your code here 
+  stat("./src/fs/files_copy", (err, stats) => {
+    if (err) {
+      fs.cp(
+        "./src/fs/files",
+        "./src/fs/files_copy",
+        { recursive: true },
+        (err) => {
+          if (err) throw err;
+        }
+      );
+    }
+    if (stats) {
+      throw new Error("FS operation failed");
+    }
+  });
 };
 
 await copy();
