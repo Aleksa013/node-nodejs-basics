f073649da51a55f2db645a70f8a7f02d9f7e29df
Aleksa013 <velma86@bk.ru>
2024-10-02 19:23:38 +0300

    feat: fs/read and list are done

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import fs from "fs";
+import { stat } from "node:fs";
+
 const remove = async () => {
-    // Write your code here 
+  stat("./src/fs/files/fileToRemove.txt", (err, stat) => {
+    if (err) throw new Error("FS operation failed");
+    if (stat) {
+      fs.unlink("./src/fs/files/fileToRemove.txt", (err) => {
+        if (err) throw err;
+      });
+    }
+  });
 };
 
 await remove();
