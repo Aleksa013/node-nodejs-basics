9912264a1194975a8bd1b90b42c2fabd6e62b7c2
Aleksa013 <velma86@bk.ru>
2024-10-02 20:57:43 +0300

    feat: cli/ args is done

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const env = process.env.npm_lifecycle_script;
+  console.log(env.split("--"));
+  env.split("--").forEach((item, index) => {
+    const part = item.split(" ");
+    if (index) {
+      console.log(`${part[0]} is ${part[1]}`);
+    }
+  });
 };
 
 parseArgs();
