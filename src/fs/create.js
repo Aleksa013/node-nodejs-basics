fb3befb6c7ccdabddda3411f842bc23882105b71
ailchenko <velma86@bk.ru>
2025-04-22 22:47:55 +0300

    feat: write function create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
-const create = async () => {
-    // Write your code here 
-};
+import { open, appendFile, access, writeFile, constants } from 'node:fs/promises';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+
+const __filePath= fileURLToPath(import.meta.url);
+const __dirName = dirname(__filePath);
+
+const create = async () => {   
+    try{
+        await access(`${__dirName}/files/fresh.txt`,constants.F_OK, 1).then(()=>{
+            console.error('FS operation failed');        
+        })
+    }catch{     
+         
+        await appendFile(`${__dirName}/files/fresh.txt`, 'I am fresh and young', {flag:'w'})    
+    }
+ }
+
+
 
 await create();
