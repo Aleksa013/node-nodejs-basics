2703423c2a8434bd94196fc9b4500411aa34c264
ailchenko <velma86@bk.ru>
2025-04-24 12:23:39 +0300

    feat: add function remove

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import { rm, access } from 'node:fs/promises';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filepath = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filepath);
+
 const remove = async () => {
-    // Write your code here 
+   try{
+    await access(`${__dirname}/files/fileToRemove.txt`);
+    await rm(`${__dirname}/files/fileToRemove.txt`)
+   }catch(error){
+    throw new Error('FS operation failed')
+   }
 };
 
 await remove();
