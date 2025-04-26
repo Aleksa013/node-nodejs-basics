f92de97294f7f7d7c1cb6489e321f768b9a4f1b9
ailchenko <velma86@bk.ru>
2025-04-26 19:12:13 +0300

    feat: add function env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
+import { env, stdout } from'node:process';
+
+
 const parseEnv = () => {
-    // Write your code here 
+   const _RSS = Object.assign({},env)
+    for ( const [key, value] of Object.entries(_RSS)) {
+        if(key.startsWith('RSS_')){
+            console.log(`${key} = ${value}`)
+        }
+    }
 };
 
 parseEnv();
