ebd7c48eeefc769635bc77c41441d518c55f4ffe
ailchenko <velma86@bk.ru>
2025-04-26 16:42:09 +0300

    feat: add strange fumction read for stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
-const read = async () => {
-    // Write your code here 
+import { fileURLToPath } from "node:url";
+import { dirname } from 'node:path';
+import { createReadStream } from "node:fs";
+import { stdout } from "node:process";
+
+
+const __filepath = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filepath);
+
+const read = async () => { 
+ const rs = createReadStream(`${__dirname}/files/fileToRead.txt`);
+    rs.on('data', (data) => stdout.write(data));
+    rs.on('error', (error) => console.error(error));
+    rs.on('end', ()=> console.log('\n'))
 };
 
-await read();
+await read()
