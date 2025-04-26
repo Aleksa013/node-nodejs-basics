f38aa8ea5579e8a3e51e0df2292741ca16712f64
ailchenko <velma86@bk.ru>
2025-04-26 17:39:14 +0300

    feat: add function transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+
+import { stdin, stdout } from "node:process";
+import { Transform } from "node:stream";
+import { pipeline } from "node:stream/promises";
+
 const transform = async () => {
-    // Write your code here 
+   const ts = new Transform({
+    transform(chunk, encoding, callback){         
+        const str = chunk.toString().split('').reverse().join('');
+        this.push(`${str}\n`)
+        callback()
+    }
+   })
+   await pipeline(stdin, ts, stdout)
 };
 
 await transform();
