d63d45bd5aa0dd398c11c2d7f0b0b39e79a76484
ailchenko <velma86@bk.ru>
2025-04-27 18:47:35 +0300

    feat: add function hash

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,11 +1,9 @@
 import { argv } from'node:process';
 
-const parseArgs = () => {
-   
+const parseArgs = () => {   
     for(let i = 2; i < argv.length; i++){
        if(argv[i].startsWith('--')) console.log(`${argv[i]} is ${argv[i+1]}`)
-    }
-    
+    }    
 };
 
 parseArgs();
--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { createReadStream } from 'node:fs';
+import { dirname } from 'node:path';
+import { stdout } from 'node:process';
+import { pipeline } from 'node:stream/promises';
+import { fileURLToPath } from 'node:url';
+const { createHash } = await import('node:crypto');
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+    try{
+        const rs = createReadStream(`${__dirname}/files/fileToCalculateHashFor.txt`)
+        const hash = createHash('sha256');
+        rs.on('data', (data) => {    
+            console.log(hash.update(data).digest('hex'))
+        })       
+    }catch(error){
+        console.log(error)
+    }
 };
 
 await calculateHash();
