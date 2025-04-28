bf76482f8561ccd2485afe41534b7a1c9d5cbbb3
ailchenko <velma86@bk.ru>
2025-04-28 09:49:22 +0300

    feat: add child_process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,23 @@
+import { fork } from 'node:child_process';
+import { dirname } from 'node:path';
+import { stdin, stdout } from 'node:process';
+import { fileURLToPath } from 'node:url';
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    try{
+       const c_p = fork(`${__dirname}/files/script.js`, args);
+       stdin.on('data', (data) => {
+        c_p.send(`To script : ${data}`)
+       })       
+       c_p.on('message', (message) => stdout.write(`From script: ${message}`))
+     
+    }catch(error){
+        console.error('Error my:'+error)
+    }
+ 
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['someArgument1', 'someArgument2'] );
