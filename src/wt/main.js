55ff81a33c45930b22854360b7e435497e33424f
ailchenko <velma86@bk.ru>
2025-04-28 15:33:20 +0300

    feat: add worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,48 @@
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { isMainThread, Worker} from 'node:worker_threads';
+import { cpus } from 'node:os';
+
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+
+const getResultWorker = async(message) => {
+    return new Promise((resolve, reject) => {
+        const worker =  new Worker(`${__dirname}/worker.js`, { workerData: {message}});
+        worker.on('message', resolve)
+        worker.on('error', reject)
+        worker.on('exit', (code)=> {
+            if(code !== 0)
+                throw new Error(code)
+        }) 
+    })        
+}
 const performCalculations = async () => {
-    // Write your code here
+    const resultArray = [];
+    if(isMainThread) {
+        for(let i = 0; i < cpus().length; i++){  
+           const result = await getResultWorker(`${i+10}`)
+           .then((data, error) => {
+            if(data){
+                return {
+                    status:'resolved',
+                    data
+                }
+            } 
+            if(error) {
+                return {
+                    status:'error',
+                    data: null
+                }
+            }
+           })
+           resultArray.push(result)
+           if(i === cpus().length - 1) console.log(resultArray)
+       }
+    }
+    
 };
 
 await performCalculations();
